2ca4d29ed2b24691239fdfd5c13262600445ba72
Amitash <amitu895@gmail.com>
2022-07-17 17:07:51 +0530

    Modified greeting message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log('Hello from my-package');
 
 function sayHello(name) {
-	console.log(`Hello ${name}!\nHave a great day!`);
+	console.log(`Hello ${name}!\nHave a great day!\nIt is nice to meet you.`);
 }
 
 module.exports = {sayHello};
