214509b35f47aceb6a069e207a3d1b6e6daf811e
Amitash <amitu895@gmail.com>
2022-07-17 17:00:56 +0530

    Changed greeting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log('Hello from my-package');
 
 function sayHello(name) {
-	console.log(`Hello ${name}!`);
+	console.log(`Hello ${name}!\nHave a great day!`);
 }
 
 module.exports = {sayHello};
